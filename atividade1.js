d1455b047b507e70e3ba7b0414c642731e0deab1
Timóteo <timoteodasilvabastos15@gmail.com>
2025-06-26 15:27:34 -0300

    feito

--- atividade1.js
+++ atividade1.js
@@ -1,2 +1,3 @@
+//Crie uma variável chamada nome e atribua o seu nome.
 let nome = "Timóteo"
 console.log(nome)
--- atividade2.js
+++ atividade2.js
@@ -1,2 +1,3 @@
+//Crie uma variável idade e atribua sua idade.
 let idade = 18
 console.log(idade)
